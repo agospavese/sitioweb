750cd017ebe6427f428d1922cc5811db9c9fd443
Agostina Pavese <agostinap85@gmail.com>
2021-08-09 18:56:09 -0300

    add: se crearon varias cosas

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const app = express();
+const path = require('path');
+app.use(express.static(path.join(__dirname, "./public")));
 app.get ('/',(req, res) => {
-    res.send('Hola Mundo')
-})
-
+    res.sendFile(path.join(__dirname, "./view/index.html"));
+});
 app.listen(3000, () => {
     console.log('Server is running on PORT : 3000')
 })
